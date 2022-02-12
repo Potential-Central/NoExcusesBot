4b7dc047289f2d29e7e718ba5e6ec3732a7bac17
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-02-12 22:35:56 +0200

    First version of the rewrite, added the bot construct

--- bot/bot.go
+++ bot/bot.go
@@ -1,11 +1,51 @@
 package NoExcusesBot
 
 import (
+	"log"
+
 	"github.com/bwmarrin/discordgo"
 )
 
+// The basic bot object
 type Bot struct {
-	Perfix  string
+	Prefix  string
 	Token   string
+	logger  *log.Logger
 	Session *discordgo.Session
+}
+
+// Takes a token and creates a discord session, initializes the bot.
+// 2nd argument is an optional prefix, default is !
+func MakeBot(token string, args ...string) (*Bot, error) {
+	var err error
+	prefix := "!"
+	if len(args) >= 1 {
+		prefix = args[0]
+	}
+	ret := &Bot{Prefix: prefix, Token: token}
+	ret.logger = log.Default()
+	ret.Session, err = discordgo.New("Bot " + token)
+	if err != nil {
+		return nil, err
+	}
+	ret.Session.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMembers | discordgo.IntentsGuildMessages
+	ret.Session.AddHandler(ret.messageHandler)
+	return ret, nil
+}
+
+//Starts the bot, this function is not blocking.
+func (bot *Bot) Start() {
+	err := bot.Session.Open()
+	if err != nil {
+		bot.logger.Fatal("[SETUP] Error opening connection,", err)
+	}
+}
+
+//Stops the bot.
+func (bot *Bot) Stop() {
+	bot.Session.Close()
+}
+
+func (bot *Bot) messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	bot.logger.Println("Test")
 }
--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	bot "github.com/Potential-Central/NoExcusesBot/bot"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	client := bot.Bot{}
-	fmt.Println(client)
+	godotenv.Load()
+	client, _ := bot.MakeBot(os.Getenv("DISCORD_TOKEN"))
+	client.Start()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	client.Stop()
 }
