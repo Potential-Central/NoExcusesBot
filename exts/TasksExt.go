f911b7f0bc6dbb94f5a905e1c5c7bc85f0c4d429
SHA65336 <53488697+SHA65536@users.noreply.github.com>
2022-02-27 19:52:04 +0200

    Added task dataobject

--- exts/TasksExt.go
+++ exts/TasksExt.go
@@ -7,6 +7,25 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// Task object implementing DataObject
+type Task struct {
+	Id           int    `json:"id"`
+	Guild        int    `json:"guild,omitempty"`
+	User         int    `json:"user,omitempty"`
+	NextReminder int    `json:"nextReminder,omitempty"`
+	Interval     int    `json:"interval,omitempty"`
+	Repeats      int    `json:"repeats,omitempty"`
+	Message      string `json:"message,omitempty"`
+}
+
+func (task Task) bucket() string {
+	return "Tasks"
+}
+
+func (task Task) primaryKey() string {
+	return strconv.Itoa(task.Id)
+}
+
 type TasksExt struct {
 	Bot      *NoExcusesBot.Bot
 	Commands []*NoExcusesBot.Command
